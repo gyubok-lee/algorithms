0964d1166e9a1dcf3ffb9056209f7ed4ebb16364
gyubok-lee <fireza@naver.com>
2021-01-06 14:27:23 +0900

    2021-01-06

--- week2/2_3.py
+++ week2/2_3.py
@@ -1,40 +1,68 @@
-def DFS(que,num,depth):
-    global local_max
+# https://www.acmicpc.net/problem/14502
+
+from collections import deque
+def finder(depth):
     global arr
+    global zeros
     global visited
 
-    if depth == 4:
-        if num > local_max :
-            local_max = num
+    if depth == 3:
+        infection(arr)
         return
 
-    now = que[-1]
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+    for _ in range(len(zeros)) :
+        if visited[_] == 0:
+            arr[zeros[_][0]][zeros[_][1]] = 1
+            visited[_] = 1
+            finder(depth+1)
+            arr[zeros[_][0]][zeros[_][1]] = 0
+            visited[_] = 0
+    return
 
+def infection (a):
+    global max_size
 
-    for i in range(4):
-        nx = now[0] + dx[i]
-        ny = now[1] + dy[i]
-        if 0<=nx<n and 0<=ny<m and visited[nx][ny]:
-            visited[nx][ny] = 0
-            que.append([nx,ny])
-            num += arr[nx][ny]
-            DFS(que,num,depth+1)
+    dir = [ (-1,0), (1,0), (0,-1), (0,1) ]
 
-    visited[now[0]][now[1]] = 1
-    que.pop()
-    num -= arr[now[0]][now[1]]
+    # 깊은 복사
+    sim= [a[i][:] for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if sim[i][j] == 2:
+                que.append([i, j])
+
+    # 시뮬레이션
+    while que:
+        x,y = que.popleft()
+        for d in dir:
+            nx, ny = x + d[0], y + d[1]
+            if 0<= nx <n and 0<= ny < m:
+                if sim[nx][ny] == 0 :
+                    sim[nx][ny] = 2
+                    que.append([nx,ny])
+    cnt = 0
+    for i in sim:
+        cnt += i.count(0)
+    max_size = max(max_size, cnt)
+
     return
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(n)]
+arr = []
+virus = []
+zeros = []
 
-total_max = 0
 for i in range(n):
+    row = list(map(int, input().split()))
     for j in range(m):
-        visited[i][j] = 0
-        DFS([[i, j]], arr[i][j], 1)
-        if total_max < local_max :
-            total_max = local_max
-print(total_max)
+        if row[j] == 2:
+            virus.append([i,j])
+        elif row[j] == 0:
+            zeros.append([i,j])
+    arr.append(row)
+
+visited = [0]* len(zeros)
+max_size = 0
+que = deque()
+finder(0)
+print(max_size)
